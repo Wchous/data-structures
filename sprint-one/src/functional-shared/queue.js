3d2559ff71419daf218a1bf50a545fa012923a43
wchous <wchoustoun@gmail.com>
2018-06-13 17:34:51 -0500

    complete queue functional shared

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,45 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var obj = {}
+  obj.qSize = 0
+  obj.keyCounter = 0
+  _.extend(obj, queueMethods)
+
+  return obj
 };
 
-var queueMethods = {};
+
+var queueMethods = {
+  enqueue: function(value){
+    this[this.keyCounter] = value
+    this.qSize++
+    this.keyCounter++
+    //console.log('e',this.qSize)
+  },
+  
+  dequeue: function(){
+    //console.log(Object.keys(this))
+    var firstKey = Object.keys(this)[0]
+    var removed = this[firstKey]
+    if (Object.keys(this).length>5){
+    delete this[firstKey]
+    }
+    // console.log('before', this.qSize)
+    if(this.qSize > 0){
+      this.qSize--
+    }
+    //console.log('d',this.qSize)
+    // console.log('after', this.qSize)
+    return removed
+  },
+
+  size: function(){
+    if(this.qSize<0){
+      return 0
+    }    
+    return this.qSize
+  }
+};
 
 
