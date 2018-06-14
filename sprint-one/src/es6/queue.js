e213c15810a8d960c33590f56818b22d70597b7d
wchous <wchoustoun@gmail.com>
2018-06-13 19:52:58 -0500

    complete es6 stack & queue

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -2,6 +2,27 @@ class Queue {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.qSize = 0;
+    this.keyCount = 0;
+  }
+  enqueue(val) {
+    this[this.keyCount] = val;
+    this.keyCount++;
+    this.qSize++;
+  }
+
+  dequeue() {
+    var first = Object.keys(this)[0];
+    var removed = this[first];
+    if(this.qSize > 0){
+      delete this[first];
+      this.qSize--;
+    }
+    return removed;
+  }
+
+  size() {
+    return this.qSize;
   }
 
 }
--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -2,6 +2,25 @@ class Stack {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.stackSize = 0;
+  }
+
+  push(val) {
+    this[this.stackSize] = val;
+    this.stackSize++;
+  }
+
+  pop() {
+    if (this.stackSize > 0) {
+      this.stackSize--;
+    }
+    var removed = this[this.stackSize];
+    delete this[this.stackSize];
+    return removed;
+  }
+
+  size() {
+    return this.stackSize;
   }
 
 }
